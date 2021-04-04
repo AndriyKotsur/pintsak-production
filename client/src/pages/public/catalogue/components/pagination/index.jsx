bafbc17d358749f477183f51277a005c1b92ed71
AdriyKotsur <kotsurandriy@gmail.com>
2021-04-04 23:35:36 +0300

    add type by filering

--- client/src/pages/public/catalogue/components/pagination/index.jsx
+++ client/src/pages/public/catalogue/components/pagination/index.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {Fragment} from 'react'
+import {Link} from 'react-router-dom'
 
-import { Icon } from 'components'
+import {Icon} from 'components'
 import classNames from 'classnames'
 import s from './style.module.scss'
 
-const Pagination = () => {
-	return (
-		<div className={s.wrapper}>
-			<Link to="" className={s.refresh}>
-				<Icon name="refresh" className={classNames('icon', 'icon-refresh', s.icon_refresh)} />
-        Показати ще 12 товарів
-			</Link>
-			<div className={s.block}>
-				<Link href="#" className={s.step}>
-					<Icon name="pagination" className={classNames('icon', 'icon-pagination', s.icon_left)} />
-				</Link>
-				<div className={s.pages}>
-					<Link to="" className={s.page}>1</Link>
-				</div>
-				<Link to="" className={s.step}>
-					<Icon name="pagination" className={classNames('icon', 'icon-pagination', s.icon_right)} />
-				</Link>
-			</div>
-		</div>
-	)
+const Pagination = ({page, pages, handlePageBy}) => {
+    return (
+        <Fragment>
+            {
+                pages > 1 &&
+                <div className={s.wrapper}>
+                    <Link to="" className={s.refresh}>
+                        <Icon name="refresh" className={classNames('icon', 'icon-refresh', s.icon_refresh)}/>
+                        Показати ще 12 товарів
+                    </Link>
+                    <div className={s.block}>
+                        <button className={classNames(s.step, {[s.disabled]: page <= 1})}
+                                onClick={() => handlePageBy(page - 1)}
+                                disabled={page <= 1}>
+                            <Icon name="pagination" className={classNames('icon', 'icon-pagination', s.icon_left)}/>
+                        </button>
+                        <div className={s.pages}>
+                            <Link to="" className={s.page}>1</Link>
+                        </div>
+                        <button className={classNames(s.step, {[s.disabled]: page >= pages})}
+                                onClick={() => handlePageBy(page + 1)}
+                                disabled={page >= pages}>
+                            <Icon name="pagination" className={classNames('icon', 'icon-pagination', s.icon_right)}/>
+                        </button>
+                    </div>
+                </div>
+            }
+        </Fragment>
+    )
 }
 
 export default Pagination
--- client/src/pages/public/catalogue/components/sort/index.jsx
+++ client/src/pages/public/catalogue/components/sort/index.jsx
@@ -1,36 +1,36 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
-import { Icon } from 'components'
+import {Icon} from 'components'
 import classNames from 'classnames'
 import s from './style.module.scss'
 
-const Sort = ({ sortBy, orderBy, handleSortBy, handleOrderBy }) => {
-	return (
-		<div className={s.wrapper}>
-			<span className={s.title}>Сортування:</span>
-			<div className={s.block}>
-				<button
-					className={s.size}
-					onClick={() => {
-						handleSortBy('sizes.width')
-						handleOrderBy(prev => prev === 1 ? -1 : 1)
-					}} >
-          за розміром
-					<Icon name='dropdown' className={classNames('icon', 'icon-sort', s.icon, {[s.active]: sortBy === 'sizes.width' && orderBy == 1})} />
-				</button>
-				<button
-					className={s.price}
-					onClick={() => {
-						handleSortBy('prices.grey')
-						handleOrderBy(prev => prev === 1 ? -1 : 1)
-					}}>
-          за ціною
-					<Icon name='dropdown' className={classNames('icon', 'icon-sort', sortBy === 'prices.grey' && orderBy === 1 && s.active)} />
-				</button>
-			</div>
-		</div>
-	)
+const Sort = ({sortBy, orderBy, handleSortBy, handleOrderBy}) => {
+    return (
+        <div className={s.wrapper}>
+            <span className={s.title}>Сортування:</span>
+            <div className={s.block}>
+                <button
+                    className={classNames(s.size, {[s.active]: sortBy === 'sizes.width' && orderBy == 1})}
+                    onClick={() => {
+                        handleSortBy('sizes.width')
+                        handleOrderBy(prev => prev === 1 ? -1 : 1)
+                    }}>
+                    за розміром
+                    <Icon name='dropdown'
+                          className={classNames('icon', 'icon-sort', s.icon, {[s.active]: sortBy === 'sizes.width' && orderBy == 1})}/>
+                </button>
+                <button
+                    className={classNames(s.price, {[s.active]: sortBy === 'prices.grey' && orderBy == 1})}
+                    onClick={() => {
+                        handleSortBy('prices.grey')
+                        handleOrderBy(prev => prev === 1 ? -1 : 1)
+                    }}>
+                    за ціною
+                    <Icon name='dropdown'
+                          className={classNames('icon', 'icon-sort', s.icon, {[s.active]: sortBy === 'prices.grey' && orderBy == 1})}/>
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Sort
