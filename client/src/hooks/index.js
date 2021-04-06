271150d5e8a7e483b2e0cd25064027a086038775
Myroslav <48691327+myroslavtokar@users.noreply.github.com>
2021-04-06 12:11:39 +0300

    Create index file for hooks

---
+++ client/src/hooks/index.js
@@ -0,0 +1,4 @@
+export { default as useCookies } from './useCookies'
+export { default as useCopyToClipboard } from './useCopyToClipboard'
+export { default as useHTTPRequest } from './useHTTPRequest'
+export { default as useLockBodyScroll } from './useLockBodyScroll'
