e701f9a43a76098f492c7c9e8938462eb1286b13
Myroslav Tokar <myroslav.tokar96@gmail.com>
2021-05-16 12:34:45 +0300

    html-pdf

--- api/server/routes/v1/public/index.js
+++ api/server/routes/v1/public/index.js
@@ -2,89 +2,89 @@ const express = require('express')
 const router = express.Router()
 const fs = require('fs')
 
-const {Type, Tile, Customer} = require('../../../models')
-const {sendMail} = require('../../../services/sendgrid')
+const { Type, Tile, Customer } = require('../../../models')
+const { sendMail } = require('../../../services/sendgrid')
 
 // get tile
 router.get('/tile/:url', async (req, res) => {
-    try {
-        const { url } = req.params
-        const tile = await Tile.findOne({ url }).populate('type')
+	try {
+		const { url } = req.params
+		const tile = await Tile.findOne({ url }).populate('type')
 
-	const tiles = await Tile.find({ url: { $ne: tile.url }, type: tile.type }).limit(9).populate('type')
+		const tiles = await Tile.find({ url: { $ne: tile.url }, type: tile.type }).limit(9).populate('type')
 
-        res.status(200).json({ success: true, data: { tile, tiles } })
-    } catch (err) {
-        res.status(404).json({ success: false, message: err.message })
-    }
+		res.status(200).json({ success: true, data: { tile, tiles } })
+	} catch (err) {
+		res.status(404).json({ success: false, message: err.message })
+	}
 })
 
 // get types
 router.get('/types', async (_, res) => {
-    try {
-        const types = await Type.find()
+	try {
+		const types = await Type.find()
 
-        res.status(200).json({ success: true, data: types })
-    } catch (err) {
-        res.status(404).json({ success: false, message: err.message })
-    }
+		res.status(200).json({ success: true, data: types })
+	} catch (err) {
+		res.status(404).json({ success: false, message: err.message })
+	}
 })
 
 // get tiles by sorting
 router.get('/tiles', async (req, res) => {
-    try {
-        const {page, type, sort, order} = req.query
-        const _page = page ? page - 1 : 0
-        const limit = 9
+	try {
+		const { page, type, sort, order } = req.query
+		const _page = page ? page - 1 : 0
+		const limit = 9
 
-        const findBy = type ? {'type.url': type} : {}
-        const sortBy = sort ? {[sort]: Number(order)} : {createdAt: -1}
+		const findBy = type ? { 'type.url': type } : {}
+		const sortBy = sort ? { [sort]: Number(order) } : { createdAt: -1 }
 
-        const tiles = await Tile
-            .aggregate([
-                {
-                    $lookup: {
-                        from: 'types',
-                        localField: 'type',
-                        foreignField: '_id',
-                        as: 'type'
-                    }
-                },
-                {
-                    $project: {
-                        _id: 1,
-                        title: 1,
-                        type: {'$arrayElemAt': ['$type', 0]},
-                        url: 1,
-                        is_popular: 1,
-                        is_available: 1,
-                        images: 1,
-                        prices: 1,
-                        sizes: 1,
-                    }
-                },
-                {
-                    $match: findBy,
-                },
-                {
-                    $sort: sortBy,
-                },
-                {
-                    $skip: _page * limit,
-                },
-                {
-                    $limit: limit,
-                }
-            ])
+		const tiles = await Tile
+			.aggregate([
+				{
+					$lookup: {
+						from: 'types',
+						localField: 'type',
+						foreignField: '_id',
+						as: 'type',
+					},
+				},
+				{
+					$project: {
+						_id: 1,
+						title: 1,
+						type: { '$arrayElemAt': ['$type', 0] },
+						url: 1,
+						is_popular: 1,
+						is_available: 1,
+						images: 1,
+						prices: 1,
+						sizes: 1,
+					},
+				},
+				{
+					$match: findBy,
+				},
+				{
+					$sort: sortBy,
+				},
+				{
+					$skip: _page * limit,
+				},
+				{
+					$limit: limit,
+				},
+			])
 
-        const filterBy = type && tiles.length > 0 ? {type: tiles[0].type._id} : {}
-        const count = await Tile.countDocuments(filterBy)
-        const pages = count ? Math.ceil(count / limit) : 0
+		const filterBy = type && tiles.length > 0 ? { type: tiles[0].type._id } : {}
+		const count = await Tile.countDocuments(filterBy)
+		const pages = count ? Math.ceil(count / limit) : 0
 
-        res.status(200).json({success: true, data: {tiles, pages}})
-    } catch (err) {
-        res.status(404).json({success: false, message: err.message})
-    }
+		res.status(200).json({ success: true, data: { tiles, pages } })
+	} catch (err) {
+		res.status(404).json({ success: false, message: err.message })
+	}
 })
 
 // get populars tiles
@@ -100,27 +100,28 @@ router.get('/popular',async (_, res) => {
 
 // download catalogue
 router.get('/download-catalogue', (_, res) => {
-    try {
-        const file = fs.readdirSync('./public/docs', function (error, files) {
-            if (error) throw error
-            return files
-        })
+	try {
+		const file = fs.readdirSync('./public/docs', function (error, files) {
+			if (error) throw error
 
-        res.status(200).download(`public/docs/${file[0]}`)
-    } catch (err) {
-        res.status(404).json({success: false, message: err.message})
-    }
+			return files
+		})
+
+		res.status(200).download(`public/docs/${file[0]}`)
+	} catch (err) {
+		res.status(404).json({ success: false, message: err.message })
+	}
 })
 
 // order request
 router.post('/order-request', async (req, res) => {
-    try {
-        const {name, phone, comment, order} = req.body
+	try {
+		const { name, phone, comment, order } = req.body
 
-        const newOrder = await Customer.create(req.body)
+		const newOrder = await Customer.create(req.body)
 
-        if (newOrder) {
-            const content =
+		if (newOrder) {
+			const content =
                 `<table>
 					<tbody>
 						<tr>Customer: ${name}</tr>
@@ -133,16 +134,16 @@ router.post('/order-request', async (req, res) => {
 					</tbody>
 				</table>`
 
-            const response = await sendMail({fromEmail: 'pintsak-tiles.com.ua', subject: 'New order request!', content})
-            if (response[0].statusCode !== 202) return res.status(400).json({success: false, message: 'Email error'})
+			const response = await sendMail({ fromEmail: 'pintsak-tiles.com.ua', subject: 'New order request!', content })
+			if (response[0].statusCode !== 202) return res.status(400).json({ success: false, message: 'Email error' })
 
-            res.status(201).json({success: true, message: 'Successfully sended'})
-        }
+			res.status(201).json({ success: true, message: 'Successfully sended' })
+		}
 
-        res.status(400).json({success: false, message: 'Something went wrong'})
-    } catch (err) {
-        res.status(400).json({success: false, message: err.message})
-    }
+		res.status(400).json({ success: false, message: 'Something went wrong' })
+	} catch (err) {
+		res.status(400).json({ success: false, message: err.message })
+	}
 })
 
 module.exports = router
