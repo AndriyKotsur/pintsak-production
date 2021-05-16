edd6ef9e673ee6e6d8943786fc6a9f6355a95c5c
Myroslav Tokar <myroslav.tokar96@gmail.com>
2021-05-16 17:48:18 +0300

    helmet config

--- api/index.js
+++ api/index.js
@@ -11,7 +11,22 @@ const app = express()
 
 // middlewares
 app.use(cors())
-app.use(helmet())
+app.use(helmet.contentSecurityPolicy({
+	useDefaults: true,
+	directives: {
+		'script-src': ["'self'", 'pintsak-production.herokuapp.com/'],
+	},
+}))
+app.use(helmet.dnsPrefetchControl())
+app.use(helmet.expectCt())
+app.use(helmet.frameguard())
+app.use(helmet.hidePoweredBy())
+app.use(helmet.hsts())
+app.use(helmet.ieNoOpen())
+app.use(helmet.noSniff())
+app.use(helmet.permittedCrossDomainPolicies())
+app.use(helmet.referrerPolicy())
+app.use(helmet.xssFilter())
 app.use(logger('dev'))
 app.use(express.json())
 app.use('/public', express.static(__dirname + '/public'))
@@ -20,11 +35,11 @@ app.use('/public', express.static(__dirname + '/public'))
 app.use('/v1', require('./server/routes/v1'))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
+	app.use(express.static('client/build'))
 
-  app.get('*', (__, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+	app.get('*', (__, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	})
 }
 
 app.listen(port, () => {
