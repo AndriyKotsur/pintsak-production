93e6604b71931b940d9a441c9848ef0d337c9a67
Myroslav Tokar <myroslav.tokar96@gmail.com>
2021-05-17 11:58:01 +0300

    uns

--- api/index.js
+++ api/index.js
@@ -14,8 +14,8 @@ app.use(cors())
 app.use(helmet.contentSecurityPolicy({
 	useDefaults: true,
 	directives: {
-		'script-src': ["'self'", 'https://pintsak-production.herokuapp.com'],
-		// 'img-src': ["'self'", 'https://pintsak-production.s3.eu-central-1.amazonaws.com', 'https://pintsak-production.herokuapp.com'],
+		'script-src': ["'self'", "https://pintsak-production.herokuapp.com 'unsafe-inline"],
+		'img-src': ["'self'", 'https://pintsak-production.s3.eu-central-1.amazonaws.com', 'https://pintsak-production.herokuapp.com'],
 	},
 }))
 app.use(helmet.dnsPrefetchControl())
