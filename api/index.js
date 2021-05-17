62ed4cb0c2782ec22ae11e536d3cbb8010aed4a0
Myroslav Tokar <myroslav.tokar96@gmail.com>
2021-05-17 11:36:35 +0300

    index

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use('/public', express.static(__dirname + '/public'))
 app.use('/v1', require('./routes/v1'))
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')))
+	app.use(express.static('client/build'))
 
 	app.get('*', (_, res) => {
 		// res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
